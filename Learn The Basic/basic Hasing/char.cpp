c59995aca3b4ee44a6ecafea3cc4aac22eafffa8
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-06-25 14:02:35 +0530

    find freq of string char

---
+++ Learn The Basic/basic Hasing/char.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int hashfun(string str, vector<int> &vec)
+{
+
+    for (int i = 0; i < str.size(); i++)
+    {
+
+        int ch = str[i] - 'a';
+
+        if (vec[ch]!=0)
+        {
+            vec[ch]++;
+        }
+        else
+        {
+            vec[ch] = 1;
+        }
+    }
+}
+
+int main()
+{
+
+    string str = "aaaabbbbczzqq";
+
+    vector<int> vec(26, 0);
+    hashfun(str, vec);
+
+    char ch = 'a';
+    for (auto c : vec)
+    {
+        cout << ch << " = " << c << endl;
+        ch++;
+    }
+    return 0;
+}
