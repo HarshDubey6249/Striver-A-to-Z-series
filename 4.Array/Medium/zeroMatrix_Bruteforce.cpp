3d4a8e36bd02f33e0cdf303bc4e53daf22047a5d
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-07-16 20:58:48 +0530

    Implement brute-force Set Matrix Zeroes solution using row and column marking with sentinel value (-1)

---
+++ 4.Array/Medium/zeroMatrix_Bruteforce.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+void makerow(vector<vector<int>> &vec,int row){
+
+    for(int j=0;j<3;j++){
+        vec[row][j]=-1;
+    }
+}
+void makecol(vector<vector<int>> &vec,int col){
+
+    for(int i=0;i<3;i++){
+        vec[i][col]=-1;
+    }
+}
+void zeroMatric( vector<vector<int>> &vec,int n){
+
+     for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+
+            if(vec[i][j]==0){
+                makerow(vec,i);
+                makecol(vec,j);
+            }
+            
+        }
+        
+    }
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+
+            if(vec[i][j]==-1){
+               vec[i][j]=0;
+            }
+            
+        }
+        
+    }
+}
+
+int main(){
+    vector<vector<int>>vec={{1,2,3},{4,0,6},{7,8,9}};
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<vec[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+cout<<endl<<"++++++++++++++++++++++++++++++++++++++++"<<endl;
+
+zeroMatric(vec,3);
+
+ for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<vec[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
