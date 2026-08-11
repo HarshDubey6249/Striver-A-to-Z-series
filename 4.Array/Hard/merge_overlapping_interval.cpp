35c038702dceed2baa0524c49b5ff13384a14768
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-08-11 23:54:12 +0530

    optimize way

--- 4.Array/Hard/merge_overlapping_interval.cpp
+++ 4.Array/Hard/merge_overlapping_interval.cpp
@@ -9,26 +9,39 @@ void overlapping_interval(vector<vector<int>> arr){
     int  n=arr.size();
 
     vector<vector<int>>ans;
-    for(int i=0;i<n;i++){
 
-        int start=arr[i][0];
-        int end=arr[i][1];
-      // cout<<i <<"-"<<start<<" "<<end<<endl;
+    // for(int i=0;i<n;i++){
 
-      if(!ans.empty() && end <= ans.back()[1]){
-        continue;
-      }
+    //     int start=arr[i][0];
+    //     int end=arr[i][1];
+    //   // cout<<i <<"-"<<start<<" "<<end<<endl;
 
-      for(int j=i+1;j<n;j++){
-        if(end >= arr[j][0]){
-            end=max(end,arr[j][1]);
-        }else{
-            break;
-        }
-      }
-      ans.push_back({start,end});
+    //   if(!ans.empty() && end <= ans.back()[1]){
+    //     continue;
+    //   }
 
+    //   for(int j=i+1;j<n;j++){
+    //     if(end >= arr[j][0]){
+    //         end=max(end,arr[j][1]);
+    //     }else{
+    //         break;
+    //     }
+    //   }
+    //   ans.push_back({start,end});
+
+    // }
+
+//----------------------------------------------
+
+for(int i=0;i<n;i++){
+
+    if(ans.empty()|| arr[i][0]> ans.back()[1]){
+        ans.push_back(arr[i]);
+    }else{
+        ans.back()[1]=max(ans.back()[1],arr[i][1]);
     }
+}
+
 
  for (auto i : ans) {
     cout << i[0] << " " << i[1] << endl;
