87ba3b191092ae8ce3e9ea2ef3a6eb818f2c4ec4
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-08-11 23:35:38 +0530

    brute force approach

---
+++ 4.Array/Hard/merge_overlapping_interval.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+void overlapping_interval(vector<vector<int>> arr){
+
+    int  n=arr.size();
+
+    vector<vector<int>>ans;
+    for(int i=0;i<n;i++){
+
+        int start=arr[i][0];
+        int end=arr[i][1];
+      // cout<<i <<"-"<<start<<" "<<end<<endl;
+
+      if(!ans.empty() && end <= ans.back()[1]){
+        continue;
+      }
+
+      for(int j=i+1;j<n;j++){
+        if(end >= arr[j][0]){
+            end=max(end,arr[j][1]);
+        }else{
+            break;
+        }
+      }
+      ans.push_back({start,end});
+
+    }
+
+ for (auto i : ans) {
+    cout << i[0] << " " << i[1] << endl;
+}
+}
+
+
+int main(){
+
+  vector<vector<int>> arr = {
+    {1, 3},
+    {2, 6},
+    {8, 10},
+    {15, 18}
+};
+
+overlapping_interval(arr);
+return 0;
+
+
+}
